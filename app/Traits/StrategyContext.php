f676d8a5001d54f063e9441c0698211a4e0468d5
vin-albertus <vinalbertus@gmail.com>
2022-04-09 16:15:44 +0700

    memisahkan strategy dengan singleton ke branch yang berbeda

--- app/Traits/StrategyContext.php
+++ app/Traits/StrategyContext.php
@@ -1,9 +1,6 @@
 <?php 
 namespace App\Traits;
 
-use App\Singleton\Singleton;
-use App\Singleton\SingletonStudentStrategyRepository;
-use App\Singleton\SingletonTeacherStrategyRepository;
 use App\Strategy\ConcreteStrategy\StudentStrategy;
 use App\Strategy\ConcreteStrategy\TeacherStrategy;
 
@@ -11,21 +8,12 @@ trait StrategyContext{
     private function setStrategyContext($user){
         switch ($user) {
             case TEACHER:
-                $this->context->setStrategy(SingletonTeacherStrategyRepository::getInstance());
+                $this->context->setStrategy(new TeacherStrategy());
                 break;
             default:
-                $this->context->setStrategy(SingletonStudentStrategyRepository::getInstance());
+                $this->context->setStrategy(new StudentStrategy());
                 break;
         }
-        
-        // switch ($user) {
-        //     case TEACHER:
-        //         $this->context->setStrategy(new TeacherStrategy());
-        //         break;
-        //     default:
-        //         $this->context->setStrategy(new StudentStrategy());
-        //         break;
-        // }
     }
 }
 
